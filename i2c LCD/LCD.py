6235f324f9df20d895a08af506b7ac29c624acb3
Jonas Carlsen <>
2019-06-15 05:29:47 +0200

    My Time/Date script
    A easy script there shows Time and Date

---
+++ i2c LCD/LCD.py
@@ -0,0 +1,17 @@
+# Import the driver
+import sys
+sys.path.append("./lib")
+import lcddriver
+import time
+
+# Initialize the lcd
+lcd = lcddriver.lcd()
+
+# Reset the lcd
+lcd.lcd_clear()
+
+# Display characters on each line
+while True:
+lcd.lcd_display_string("Time: %s" %time.strftime("%H:%M:%S"), 1)
+
+lcd.lcd_display_string("Time: %s" %time.strftime("%d:%m:%Y"), 2)
