90c482dd2b2d134c3d23a526ae4291c875612005
JJakaJonas <10506550+JJakaJonas@users.noreply.github.com>
2019-11-25 04:23:31 +0100

    Update Update.sh

--- OS/Update.sh
+++ OS/Update.sh
@@ -11,8 +11,8 @@
 #                  a crontab entry. 
 #
 
-sudo apt-get update && sudo apt-get upgrade -y 
+sudo apt-get update && sudo apt-get dist-upgrade -y 
 sudo rpi-update -y 
-sudo apt-get autoremove 
+sudo apt-get autoremove -y 
 sudo apt-get autoclean 
 sudo reboot
